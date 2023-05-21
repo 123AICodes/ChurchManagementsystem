b4fb20dc55fa9d298f279dd64ac51135d9e917f0
Adu Isaac <109229988+123AICodes@users.noreply.github.com>
2023-05-21 03:39:27 +0000

    Update constants.php

--- auth/config/constants.php
+++ auth/config/constants.php
@@ -4,5 +4,5 @@ session_start();
 define('ROOT_URL', 'http://localhost/app/webapp/');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'PkUBS6S8s!dUXp7');
+define('DB_PASS', '');
 define('DB_NAME', 'cms');
